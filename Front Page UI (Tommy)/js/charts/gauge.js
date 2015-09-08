a2af1659de26bf36faf74d342a71645c5b29b18a
Frank Leung <kleu3551@uni.sydney.edu.au>
2015-09-08 03:44:41 +0000

    the html that load the graph

---
+++ Front Page UI (Tommy)/js/charts/gauge.js
@@ -0,0 +1,24 @@
+      google.load('visualization', '1.1', {packages: ['gauge']});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Label', 'Value'],
+          ['Sleep Score', 80],
+          ['Emotions Score', 60],
+          ['Activity Score', 70]
+        ]);
+
+        var options = {
+          width: 400, height: 120,
+          redFrom: 0, redTo: 35,
+          yellowFrom:35, yellowTo: 60,
+          greenFrom:60, greenTo: 100,
+          minorTicks: 5
+        };
+
+        var chart = new google.visualization.Gauge(document.getElementById('gauge_div'));
+
+        chart.draw(data, options);
+
+        
+      }
