f2618f1e3bd81ef9006cfba98ac8ec05662d6310
Chris George <cgeo3024@uni.sydney.edu.au>
2015-09-20 15:04:17 +1000

    basic reply-only messaging system

---
+++ messaging/js/startslide.js
@@ -0,0 +1,11 @@
+jQuery("#layerslider").layerSlider({
+				pauseOnHover : false,
+				autoPlayVideos : false,
+				skinsPath : 'skins/'
+			});
+			
+jQuery("#layerslider2").layerSlider({
+				pauseOnHover : true,
+				autoPlayVideos : false,
+				skinsPath : 'skins/'
+			});
