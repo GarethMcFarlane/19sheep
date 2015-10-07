dcd58878be3cc8366af744b465d40aeca2778232
GarethMcFarlane <gmcf8558@uni.sydney.edu.au>
2015-10-07 11:17:01 +1100

    Added test table for displaying commitments (not commitments tree)

--- sheep/commitments.php
+++ sheep/commitments.php
@@ -4,107 +4,107 @@
 
 <!DOCTYPE html>
 <?php
-	session_start();
-	require_once("functions.php");
+session_start();
+require_once("functions.php");
 ?>
 
 <html lang="en">
 
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="">
-		<meta name="author" content="">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
 
-		<title>19 Sheep - Commitment</title>
+	<title>19 Sheep - Commitment</title>
 
-		<!-- Bootstrap Core CSS -->
-		<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<!-- Bootstrap Core CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
 
-		<!--//Fonts -->
-		<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-		<link href="css/nivo-lightbox.css" rel="stylesheet" />
-		<link href="css/nivo-lightbox-theme/default/default.css" rel="stylesheet" type="text/css" />
-		<link href="css/animate.css" rel="stylesheet" />
-		<!-- //Squad theme CSS -->
+	<!--//Fonts -->
+	<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link href="css/nivo-lightbox.css" rel="stylesheet" />
+	<link href="css/nivo-lightbox-theme/default/default.css" rel="stylesheet" type="text/css" />
+	<link href="css/animate.css" rel="stylesheet" />
+	<!-- //Squad theme CSS -->
 
-		<link href="color/default.css" rel="stylesheet">
-		<link href="css/social-buttons.css" rel="stylesheet">
-		<script src="js/jquery-latest.min.js" type="text/javascript"></script>
-		<script src="js/memuscript.js"></script>
-		<link rel="stylesheet" href="css/memustyles.css">
-		<link href="css/style.css" rel="stylesheet">
-		<link href="styles.css" type="text/css" rel="stylesheet">	
-	</head>
+	<link href="color/default.css" rel="stylesheet">
+	<link href="css/social-buttons.css" rel="stylesheet">
+	<script src="js/jquery-latest.min.js" type="text/javascript"></script>
+	<script src="js/memuscript.js"></script>
+	<link rel="stylesheet" href="css/memustyles.css">
+	<link href="css/style.css" rel="stylesheet">
+	<link href="styles.css" type="text/css" rel="stylesheet">	
+</head>
 
-	<?php
-	if (!empty($_POST)) {
-		if (empty($_POST['targetperday'])) {
-            die("Please enter a goal per day");
-			exit();
-        }
-        if (empty($_POST['deadline'])) {
-            die("Please enter a deadline");
-			exit();
-        }
-        if (empty($_POST['text'])) {
-            die("Please enter text");
-			exit();
-        }
-		if (empty($_POST['type'])) {
-            die("Please enter the type of goal  you want to achieve");
-			exit();
-        }
-		
+<?php
+if (!empty($_POST)) {
+	if (empty($_POST['targetperday'])) {
+		die("Please enter a goal per day");
+		exit();
+	}
+	if (empty($_POST['deadline'])) {
+		die("Please enter a deadline");
+		exit();
+	}
+	if (empty($_POST['text'])) {
+		die("Please enter text");
+		exit();
+	}
+	if (empty($_POST['type'])) {
+		die("Please enter the type of goal  you want to achieve");
+		exit();
+	}
+
 	$query = " INSERT INTO commitments (targetperday, startday, deadline, text, isachieved, type, email) 
-			   VALUES (:email, :targetperday, CURDATE(), :deadline, :text, 0, :type)";
+	VALUES (:email, :targetperday, CURDATE(), :deadline, :text, 0, :type)";
 	
-	 
-	 $query_params = array(
-        ':targetperday' => $_POST['targetperday'],
+
+	$query_params = array(
+		':targetperday' => $_POST['targetperday'],
 		':deadline' => $_POST['deadline'],
 		':text' => $_POST['text'],
 		':type' => $_POST['type'],
 		':email' => $_SESSION['email']
-    );
+		);
 	
 	try {
-        $stmt = $db->prepare($query);
-        $result = $stmt->execute($query_params);
-    } catch (PDOException $ex) {
-        die("Failed to run query: " . $ex->getMessage());
+		$stmt = $db->prepare($query);
+		$result = $stmt->execute($query_params);
+	} catch (PDOException $ex) {
+		die("Failed to run query: " . $ex->getMessage());
 		exit();
-    }
+	}
 	
 }
 ?>
-	<body>
+<body>
 
-		<script type="text/javascript" src="functionsJquery.js"></script>
-		<body data-spy="scroll">
+	<script type="text/javascript" src="functionsJquery.js"></script>
+	<body data-spy="scroll">
 		<div class="container  header-main">
 			<div class="icon">
 				<img src="img/index/19sleep.png" style="width: 50px; height: 50px;" class="icon">
 			</div>
 			<div id='cssmenu'>
-			<ul>
-				<li>
-					<a href="profile.php">Dashboard</a>
-				</li>
-				<li>
-					<a href='mood.php'>Mood Logger</a>
-				</li>
-				<li>
-					<a href='dreamdetail.php'>Dream Analysis</a>
-				</li>
-				<li>
-					<a href='commitments.php'>My commitment</a>
-				</li>
-				<li>
-					<a href='message.php'>Messages</a>
-				</li>
-			</ul>
-		</div>
+				<ul>
+					<li>
+						<a href="profile.php">Dashboard</a>
+					</li>
+					<li>
+						<a href='mood.php'>Mood Logger</a>
+					</li>
+					<li>
+						<a href='dreamdetail.php'>Dream Analysis</a>
+					</li>
+					<li>
+						<a href='commitments.php'>My commitment</a>
+					</li>
+					<li>
+						<a href='message.php'>Messages</a>
+					</li>
+				</ul>
+			</div>
 		</div>
 		
 		<header>COMMITMENTS PAGE</header>
@@ -149,29 +149,55 @@
 					</div>
 					<div id="treeLine"></div>
 					<div class="commitDivsStyle" id="commitDivs"></div>
-					<div class="noneSubmitStyle" id="noneSubmit">You have not submitted any committments</div>
-					
-					<?php
-					$query2 = "SELECT * FROM commitments WHERE (email = :email)";
+					<div class="noneSubmitStyle" id="noneSubmit">Previous commitments:</div>
+					<table class="table">
+						<thead>
+							<tr>
+								<td>Id</td>
+								<td>Target Per Day</td>
+								<td>Start Day</td>
+								<td>Deadline</td>
+								<td>Details</td>
+								<td>Achieved?</td>
+								<td>Type</td>
+								<td>Email address</td>
+							</tr>
+						</thead>
+						<tbody>
+							<?php
+							$query2 = "SELECT * FROM commitments WHERE (email = :email)";
 
 
-					$query_params2 = array(
-						':email' => $_SESSION['email']
-						);
+							$query_params2 = array(
+								':email' => $_SESSION['email']
+								);
 
-					try {
-						$stmt2 = $db->prepare($query2);
-						$result2 = $stmt2->execute($query_params2);
-					} catch (PDOException $ex) {
-						die("Failed to run query: " . $ex->getMessage());
-						exit();
-					}
+							try {
+								$stmt2 = $db->prepare($query2);
+								$result2 = $stmt2->execute($query_params2);
+							} catch (PDOException $ex) {
+								die("Failed to run query: " . $ex->getMessage());
+								exit();
+							}
 
-					$commitments = $stmt2->fetchAll();
-					var_dump($commitments);
-					?>
-				</div>
-			</div>
-		</section>
-	</body>
-</html>
+							while ($row = $stmt2->fetch()) {
+								?>
+
+								<tr>
+									<td><?php echo $row['id']?></td>
+									<td><?php echo $row['targetperday']?></td>
+									<td><?php echo $row['startday']?></td>
+									<td><?php echo $row['deadline']?></td>
+									<td><?php echo $row['text']?></td>
+									<td><?php echo $row['isachieved']?></td>
+									<td><?php echo $row['type']?></td>
+									<td><?php echo $row['email']?></td>
+								</tr>
+								<?php
+							}
+							?>
+						</div>
+					</div>
+				</section>
+			</body>
+			</html>
